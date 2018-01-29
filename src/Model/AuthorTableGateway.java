2e127c3d20cf1af60185c1574d07d726e42678f5
Kevin Gonzales <KevinGonzalesVargas@gmail.com>
2018-01-29 15:38:32 -0600

    authorModels

---
+++ src/Model/AuthorTableGateway.java
@@ -0,0 +1,11 @@
+package Model;
+
+import java.util.ArrayList;
+
+public class AuthorTableGateway {
+	public ArrayList<AuthorModel> getAuthors(){
+		ArrayList<AuthorModel> authors = new ArrayList<AuthorModel>();
+		
+		return authors;
+	}
+}
